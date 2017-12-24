a0628cfe4f64b2a6b8e5edbabeb9b55f3767b6a6
Volcomix <volcomix@gmail.com>
2017-12-24 11:18:00 +0100

    Init Enzyme for shallow testing

--- src/components/Diagram.spec.js
+++ src/components/Diagram.spec.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { shallow } from 'enzyme'
 import Diagram from './Diagram'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(
-    <MuiThemeProvider>
-      <Diagram />
-    </MuiThemeProvider>,
-    div,
-  )
+  shallow(<Diagram />)
 })
--- src/containers/App.spec.js
+++ src/containers/App.spec.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { shallow } from 'enzyme'
 import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(
-    <MuiThemeProvider>
-      <App />
-    </MuiThemeProvider>,
-    div,
-  )
+  shallow(<App />)
 })
