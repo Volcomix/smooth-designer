ecd3de16354ecfe839b3a86609189704aa2fd515
Volcomix <volcomix@gmail.com>
2017-12-26 17:18:50 +0100

    Init block detail style

--- src/components/BlockDetail.js
+++ src/components/BlockDetail.js
@@ -1,10 +1,17 @@
 //@flow
 import React from 'react'
+import { Card, CardTitle } from 'material-ui/Card'
+import TextField from 'material-ui/TextField'
+import './BlockDetail.css'
 
 export type Props = {
   name: string,
 }
 
-const BlockDetail = ({ name }: Props) => <div>Name: {name}</div>
+const BlockDetail = ({ name }: Props) => (
+  <Card className="BlockDetail">
+    <CardTitle title={<TextField hintText="Name" value={name} />} />
+  </Card>
+)
 
 export default BlockDetail
