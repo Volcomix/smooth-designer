c7d6f97fac443284ced97c1a4d1c881fd765051a
Volcomix <volcomix@gmail.com>
2017-12-27 17:54:28 +0100

    Update blocks positions on simulation tick

--- src/reducers/blocks.js
+++ src/reducers/blocks.js
@@ -1,18 +1,28 @@
 //@flow
 import type { Block } from '../types'
-import type { BlockAction } from '../actions/blockActions'
-import { ADD_BLOCK } from '../constants/actionTypes'
+import type { Action } from '../actions'
+import { ADD_BLOCK, UPDATE_FORCE } from '../constants/actionTypes'
 
 export type BlockState = Block[]
 const initialState: BlockState = []
 
+const addTodo = (state, action) => [...state, { name: '', x: 0, y: 0 }]
+
+const updateBlocksPositions = (state, action) =>
+  state.map((block, index) => {
+    const position = action.blocks[index]
+    return { ...block, x: position.x, y: position.y }
+  })
+
 const blocks = (
   state: BlockState = initialState,
-  action: BlockAction,
+  action: Action,
 ): BlockState => {
   switch (action.type) {
     case ADD_BLOCK:
-      return [...state, { name: '', x: 0, y: 0 }]
+      return addTodo(state, action)
+    case UPDATE_FORCE:
+      return updateBlocksPositions(state, action)
     default:
       return state
   }
--- src/reducers/blocks.spec.js
+++ src/reducers/blocks.spec.js
@@ -1,6 +1,7 @@
 //@flow
 import blocks from './blocks'
 import { addBlock } from '../actions/blockActions'
+import { updateForce } from '../actions/forceActions'
 
 it('returns the initial state', () => {
   expect(blocks(undefined, { type: '@@INIT' })).toEqual([])
@@ -12,4 +13,16 @@ it('adds blocks', () => {
     { name: 'Block', x: 0, y: 0 },
     { name: '', x: 0, y: 0 },
   ])
+})
+
+it('updates blocks positions', () => {
+  expect(
+    blocks(
+      [{ name: 'Block 1', x: 1, y: 1 }, { name: 'Block 2', x: 2, y: 2 }],
+      updateForce([
+        { index: 0, name: 'Block 1', x: 3, y: 3 },
+        { index: 1, name: 'Block 2', x: 4, y: 4 },
+      ]),
+    ),
+  ).toEqual([{ name: 'Block 1', x: 3, y: 3 }, { name: 'Block 2', x: 4, y: 4 }])
 })
