01b3124b5285eefa721b7be9f2536dc079c5597b
Volcomix <volcomix@gmail.com>
2018-01-02 19:11:46 +0100

    #110 Add a start linking block selector in root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@
 import { combineReducers } from 'redux'
 import blocks, { type BlocksState } from './blocks'
 import links, { type LinksState } from './links'
+import type { Block } from '../types'
 
 export type State = {
   +blocks: BlocksState,
@@ -9,5 +10,11 @@ export type State = {
 }
 
 const reducer = combineReducers({ blocks, links })
+
+export const linkingFromBlock = ({ blocks, links }: State): ?Block => {
+  if (links.linkingFromBlock !== undefined) {
+    return blocks[links.linkingFromBlock]
+  }
+}
 
 export default reducer
--- src/reducers/index.spec.js
+++ src/reducers/index.spec.js
@@ -1,6 +1,32 @@
 //@flow
-import reducer from '.'
+import reducer, { linkingFromBlock } from '.'
 
-it('combines all reducers', () => {
-  expect(reducer(undefined, {})).toEqual({ blocks: {}, links: { links: {} } })
+describe('root reducer', () => {
+  it('combines all reducers', () => {
+    expect(reducer(undefined, {})).toEqual({ blocks: {}, links: { links: {} } })
+  })
+})
+
+describe('linkingFromBlock', () => {
+  it('returns the start block if linking', () => {
+    expect(
+      linkingFromBlock({
+        blocks: {
+          '0': { id: '0', name: 'Block', x: 0, y: 0, width: 0, height: 0 },
+        },
+        links: { links: {}, linkingFromBlock: '0' },
+      }),
+    ).toEqual({ id: '0', name: 'Block', x: 0, y: 0, width: 0, height: 0 })
+  })
+
+  it('returns undefined if not linking', () => {
+    expect(
+      linkingFromBlock({
+        blocks: {
+          '0': { id: '0', name: 'Block', x: 0, y: 0, width: 0, height: 0 },
+        },
+        links: { links: {} },
+      }),
+    ).not.toBeDefined()
+  })
 })
