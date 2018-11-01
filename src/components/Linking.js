98885aa55530b97da07822e2e37b5833fe515ad3
Volcomix <volcomix@gmail.com>
2018-11-01 10:08:38 +0100

    Fix linking when browser is zoomed

--- src/components/Linking.js
+++ src/components/Linking.js
@@ -8,16 +8,23 @@ export type Props = {
   toMouse: { x: number, y: number },
 }
 
-const Linking = ({ fromBlock, toMouse }: Props) => (
-  <svg className="Linking">
-    <line
-      className="Linking-link"
-      x1={fromBlock.x}
-      y1={fromBlock.y}
-      x2={toMouse.x}
-      y2={toMouse.y}
-    />
-  </svg>
-)
+const Linking = ({ fromBlock, toMouse }: Props) => {
+  const { x: x1, y: y1 } = fromBlock
+  const { x: x2, y: y2 } = toMouse
+  return (
+    <svg
+      className="Linking"
+      style={{
+        left: Math.min(x1, x2),
+        top: Math.min(y1, y2),
+        width: Math.max(Math.abs(x2 - x1), 1),
+        height: Math.max(Math.abs(y2 - y1), 1),
+        transform: `scale(${x2 > x1 ? 1 : -1}, ${y2 > y1 ? 1 : -1})`,
+      }}
+    >
+      <line className="Linking-link" x1={0} y1={0} x2="100%" y2="100%" />
+    </svg>
+  )
+}
 
 export default Linking
